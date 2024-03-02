dad2d863e1bfba715bdac9b776fdf171837b8d9e
Fanji Arief <arieffanji@gmail.com>
2024-03-02 09:30:37 +0700

    fix: modify BadRequestException and errorHandler on app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { BadRequestException } from "./exceptions/BadRequestException.js";
-import { NotFoundException } from "./exceptions/NotFoundException.js";
+import { errorHandler } from "./exceptions/handler.js";
+import { notFoundErrorHandler } from "./exceptions/handler.js";
 import router from "./routes/api/index.js";
 import dotenv from "dotenv";
 import express from "express";
@@ -10,19 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.use(function (req, res, next) {
-  throw new NotFoundException("URL not found.");
-});
-app.use(function (err, req, res, next) {
-  if (err instanceof BadRequestException) {
-    return res.status(400).json({ message: err.message });
-  }
+app.use(notFoundErrorHandler);
 
-  if (err instanceof NotFoundException) {
-    return res.status(404).json({ message: err.message });
-  }
-
-  return res.status(500).json({ message: err.message });
-});
+app.use(errorHandler);
 
 app.listen(process.env.PORT);
